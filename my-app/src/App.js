0abdecb54eb01011bd0fcc2dba3e5c4f8b000997
Valdemar <nikiplaycar@gmail.com>
2024-10-25 17:14:42 +0300

    feat: add task blocks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import About from './components/pages/About/About';
 import ItemsList from './components/pages/ItemList/ItemList';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/items" element={<ItemsList />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- my-app/src/components/pages/Home/Home.js
+++ my-app/src/components/pages/Home/Home.js
@@ -1,4 +1,4 @@
 export default function Home() {
-    return <div>Home Page</div>;
+    return <div><p>Отладка: компонент Home рендерится корректно.</p></div>;
   }
   
